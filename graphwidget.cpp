903a4f4d539b17ae06ee046288230606c47aea4a
Wolfgang Trummer <wolfgang.trummer@t-online.de>
2023-12-21 22:11:22 +0100

    Fixed wrong head version focus in case of --all tagged

--- graphwidget.cpp
+++ graphwidget.cpp
@@ -422,8 +422,8 @@ Version* GraphWidget::gitlogSingle(QString _hash, bool _create)
     if (remotes)
         cmd += " --remotes";
 
-    if (all)
-        cmd += " --all";
+    //if (all)
+    //cmd += " --all";
 
     if (_hash.isEmpty() == false)
         cmd += " " + _hash;
@@ -943,6 +943,12 @@ void GraphWidget::diffStagedChanges()
     fromVersions.insert(localHeadVersion);
     toVersion = NULL;
 
+    // ensure visibility
+    if (localHeadVersion->ensureUnfolded())
+    {
+        updateGraphFolding();
+    }
+
     fillCompareWidgetFromToInfo();
     mwin->getToDateLabel()->setText(QString("Staged Changes"));
 
@@ -954,6 +960,9 @@ void GraphWidget::diffStagedChanges()
     // cursor
     fromToInfo->setFromToPosition(fromVersions, NULL);
     fromToInfo->show();
+
+    // focus
+    displayHits(localHeadVersion);
 }
 
 void GraphWidget::viewThisVersion(Version* _v)
@@ -983,6 +992,13 @@ void GraphWidget::diffLocalChanges()
 
     fromVersions = QSet<Version*>();
     fromVersions.insert(localHeadVersion);
+
+    // ensure visibility
+    if (localHeadVersion->ensureUnfolded())
+    {
+        updateGraphFolding();
+    }
+
     toVersion = NULL;
 
     fillCompareWidgetFromToInfo();
@@ -996,6 +1012,9 @@ void GraphWidget::diffLocalChanges()
     // cursor
     fromToInfo->setFromToPosition(fromVersions, NULL);
     fromToInfo->show();
+
+    // focus
+    displayHits(localHeadVersion);
 }
 
 void GraphWidget::compareVersions(Version* _v1, Version* _v2, bool _showDiff)
